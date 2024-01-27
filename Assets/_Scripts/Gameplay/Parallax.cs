9cf240febddd332c170a3d1e12ab168d5bfb32e3
Utkucan AY <utkucnay@gmail.com>
2024-01-27 22:17:54 +0300

    Update Parallax.cs

--- Assets/_Scripts/Gameplay/Parallax.cs
+++ Assets/_Scripts/Gameplay/Parallax.cs
@@ -27,7 +27,7 @@ public class Parallax : MonoBehaviour
                 var pos = t.transform.position;
                 //pos.x += 5800;
                 pos.x += 958 * 6;
-                t.transform.position = pos;
+                t.transform.position = pos * Time.deltaTime;
             }
         }
 
@@ -40,7 +40,7 @@ public class Parallax : MonoBehaviour
                 var pos = t.transform.position;
                 //pos.x += 5800;
                 pos.x += 958 * 6;
-                t.transform.position = pos;
+                t.transform.position = pos * Time.deltaTime;
             }
         }
 
@@ -53,7 +53,7 @@ public class Parallax : MonoBehaviour
                 var pos = t.transform.position;
                 //pos.x += 5800;
                 pos.x += 958 * 6;
-                t.transform.position = pos;
+                t.transform.position = pos * Time.deltaTime;
             }
         }
 
@@ -66,7 +66,7 @@ public class Parallax : MonoBehaviour
                 var pos = t.transform.position;
                 //pos.x += 5800;
                 pos.x += 958 * 6;
-                t.transform.position = pos;
+                t.transform.position = pos * Time.deltaTime;
             }
         }
 
@@ -79,7 +79,7 @@ public class Parallax : MonoBehaviour
                 var pos = t.transform.position;
                 //pos.x += 5800;
                 pos.x += 958 * 6;
-                t.transform.position = pos;
+                t.transform.position = pos * Time.deltaTime;
             }
         }
     }
