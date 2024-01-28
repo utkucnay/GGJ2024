add46605d57fc85cd66c923a6951c8c5eee84d6a
Gökay Tacal <gkaytacal98@gmail.com>
2024-01-28 09:12:57 +0300

    baristabey

---
+++ Assets/_Scripts/Gameplay/Barista.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barista : Entity
+{
+    public override void Execute()
+    {
+        base.Execute();
+
+        int sum = 0;
+
+        for (int i = 0; i < entitiesInArea.Length; i++)
+        {
+            if (entitiesInArea[i].GetType().Name == "Dad" || entitiesInArea[i].GetType().Name == "Mom")
+            {
+                return;
+            }
+        }
+
+        if (Random.Range(0f, 1f) < 0.05f)
+        {
+            sum -= 70;
+            GoldManager.instance.gold += sum;
+            return;
+        }
+        else
+        {
+            for (int i = 0; i < entitiesInArea.Length; i++)
+            {
+                if (entitiesInArea[i].GetType().Name == "Festival")
+                {
+                    sum += 60;
+                    GoldManager.instance.gold += sum;
+                    return;
+                }
+            }
+
+            for (int i = 0; i < entitiesInArea.Length; i++)
+            {
+                if (entitiesInArea[i].GetType().Name == "Disco")
+                {
+                    sum += 30;
+                    GoldManager.instance.gold += sum;
+                    return;
+                }
+            }
+
+            sum += 20;
+            GoldManager.instance.gold += sum;
+            return;
+        }
+    }
+}
