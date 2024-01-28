c9f7a2ab990929501e3401a83774a87ee7ce9d7a
tugaep <74678926+tugaep@users.noreply.github.com>
2024-01-28 16:13:49 +0300

    OVEROVER

---
+++ Assets/_Scripts/Gameplay/GameEnd.cs
@@ -0,0 +1,23 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameEnd : MonoBehaviour
+{
+    public Image image;
+
+    private void Awake()
+    {
+        var seq = DOTween.Sequence();
+        seq.Append(image.DOFade(1, .8f).SetEase(Ease.InOutSine));
+        seq.AppendInterval(2f);
+        seq.Append(image.DOFade(0, .6f).SetEase(Ease.InOutQuint));
+        seq.AppendCallback(() =>
+        {
+            SceneManager.LoadScene("MainMenuScene");
+        });
+    }
+}
